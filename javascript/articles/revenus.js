e5bf59a9891127ef93001c869f904ed8378584fa
PG-studio <131386895+PG-studio@users.noreply.github.com>
2023-04-21 00:46:50 +0200

    Create revenus.js

---
+++ javascript/articles/revenus.js
@@ -0,0 +1,42 @@
+let revenus = [
+    { 
+        titre: "Le RSA aujourd'hui", 
+        date: 25,
+        images: 1,
+        artc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quibusdam voluptatem aperiam molestias dolores, dolorem exercitationem facere? Quod quis voluptate, obcaecati velit sed eius ut illo dolor minus tenetur. Aspernatur!",
+        link: "https://www.caf.fr/allocataires/aides-et-demarches/droits-et-prestations/vie-professionnelle/le-revenu-de-solidarite-active-rsa"
+    },
+
+    { 
+        titre: "Bob", 
+        date: 30,
+        images: 1,
+    },
+
+    { 
+        titre: "Charlie", 
+        date: 35,
+        images: 1,
+    },
+
+    { 
+        titre: "Alice", 
+        date: 25,
+        images: 1,
+        artc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quibusdam voluptatem aperiam molestias dolores, dolorem exercitationem facere? Quod quis voluptate, obcaecati velit sed eius ut illo dolor minus tenetur. Aspernatur!",
+
+    },
+
+    { 
+        titre: "Bob", 
+        date: 30,
+        images: 1,
+    },
+
+    { 
+        titre: "Charlie", 
+        date: 35,
+        images: 1,
+    }
+];
+
